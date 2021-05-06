cb3ed806c5d63d9b41d1fa28cdf73253d7602e4d
Kaylindaww <79210939+Kaylindaww@users.noreply.github.com>
2021-05-06 12:21:41 +0300

    Add files via upload

---
+++ mywork.kt
@@ -0,0 +1,21 @@
+fun main(){
+introduction("Kay",21)
+}
+fun introduction(name:String,age:Int){
+    println("My name is $name I am $age years old")
+}
+fun students(students:String,age: Int):String{
+    if (age<=6){
+        println("serve milk")
+    }else {
+        if (age>=6 }$$ age<=15){
+        println("server fanta")
+    }else{
+        println("serve cococola")
+    }
+}
+}
+fun names(name: String):String{
+
+if (names.length==4)
+    }
